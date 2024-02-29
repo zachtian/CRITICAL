1160cb110412832826146a08ca85098e65fa94ed
Kethan Reddy <k.reddy@imperial.ac.uk>
2024-02-29 16:24:10 +0000

    Fixed risk metrics

--- highway_env/envs/llm_env.py
+++ highway_env/envs/llm_env.py
@@ -61,10 +61,12 @@ class LLMEnv(AbstractEnv):
             }
         )
         return config
+    
 
     def _reset(self) -> None:
         self._create_road()
         self._create_vehicles()
+
 
     def _create_road(self) -> None:
         """Create a road composed of straight adjacent lanes."""
@@ -75,6 +77,7 @@ class LLMEnv(AbstractEnv):
             np_random=self.np_random,
             record_history=self.config["show_trajectories"],
         )
+
 
     def _create_vehicles(self) -> None:
         total_vehicles = self.config["vehicles_count"]
@@ -141,6 +144,7 @@ class LLMEnv(AbstractEnv):
         reward *= rewards["on_road_reward"]
         return reward
 
+
     def step(self, action: Action) -> Tuple[Observation, float, bool, bool, dict]:
         # Existing code...
         self._simulate(action)
@@ -199,6 +203,7 @@ class LLMEnv(AbstractEnv):
 
         return obs, reward, terminated, truncated, info
 
+
     def _rewards(self, action: Action) -> Dict[Text, float]:
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
         lane = (
@@ -218,6 +223,7 @@ class LLMEnv(AbstractEnv):
             "on_road_reward": float(self.vehicle.on_road),
         }
 
+
     def _is_terminated(self) -> bool:
         """The episode is over if the ego vehicle crashed."""
         return (
@@ -226,9 +232,11 @@ class LLMEnv(AbstractEnv):
             and not self.vehicle.on_road
         )
 
+
     def _is_truncated(self) -> bool:
         """The episode is truncated if the time limit is reached."""
         return self.time >= self.config["duration"]
+
 
     def update_env_config(self, new_config):
         """
@@ -236,48 +244,123 @@ class LLMEnv(AbstractEnv):
 
         :param new_config: A dictionary containing new configuration parameters.
         """
-
         #new_config = ast.literal_eval(new_config)
-
         self.config.update(new_config)
         print("FINAL UPDATED CONFIG PLEASE DOUBLE CHECK", self.config)
 
-    def calculate_long_and_lat_risk_indices(self, ego_vehicle: Vehicle, other_vehicle: Vehicle) -> Tuple[float, float, float]:
-        # Constants from IDMVehicle settings
-        rho = IDMVehicle.TIME_WANTED  # Response time of the rear vehicle
-        a_max_brake = IDMVehicle.COMFORT_ACC_MIN  # Desired maximum deceleration
-        distance_wanted = 1.5  # Desired jam distance to the front vehicle
 
-        # Relative parameters for longitudinal dynamics
-        v_r = ego_vehicle.speed  # Velocity of the rear vehicle (ego vehicle)
-        v_f = other_vehicle.speed  # Velocity of the front vehicle
-        d_lon = other_vehicle.position[0] - ego_vehicle.position[0] - other_vehicle.LENGTH  # Longitudinal distance
+    def get_lon_and_lat_distance(self, ego_vehicle: Vehicle, other_vehicle: Vehicle):
+        # Fetching position of vehicles 
+        ego_vehicle_pos = ego_vehicle.position
+        other_vehicle_pos = other_vehicle.position
+        # Retrieving longitudinal and latitudinal distances 
+        lon_distance_between_vehicles = abs(ego_vehicle_pos[0] - other_vehicle_pos[0]) 
+        lat_distance_between_vehicles = abs(ego_vehicle_pos[1] - other_vehicle_pos[1])
 
-        # Safe longitudinal distance calculation
-        d_lon_min = max(v_r * rho + distance_wanted + (v_r**2) / (2 * abs(a_max_brake)) - (v_f**2) / (2 * abs(a_max_brake)), 0)
+        return lon_distance_between_vehicles, lat_distance_between_vehicles
 
-        # Longitudinal risk index calculation
-        if d_lon >= d_lon_min:
-            r_lon = 0
-        else:
-            r_lon = 1 - d_lon / d_lon_min
 
-        # Relative parameters for lateral dynamics
-        lateral_distance_wanted = 1  # Placeholder
-        lateral_velocity = abs(ego_vehicle.velocity[1])  # Lateral speed of the ego vehicle, placeholder calculation
-        lateral_distance = abs(ego_vehicle.position[1] - other_vehicle.position[1])  # Lateral distance
-
-        # Safe lateral distance calculation
-        lateral_distance_safe = lateral_distance_wanted + lateral_velocity * rho
-
-        # Lateral risk index calculation
-        if lateral_distance >= lateral_distance_safe:
-            r_lat = 0
-        else:
-            r_lat = 1 - lateral_distance / lateral_distance_safe
-
-        return r_lon, r_lat
+    def calculate_safe_lon_distance(self, ego_vehicle: Vehicle, other_vehicle: Vehicle):
+        """
+        Safe longitudinal distance 
+        *** All speed and acceleration inputs must be for the longitudinal axis ***
     
+        Ego is assumed to be rear vehicle.
+        response_time: time it takes rear car to react and begin braking --> ASSUMED 0.1 (NEAR INSTANTANEOUS)
+        v_ego_x: current longitudinal speed of rear car
+        v_other_x: current longitudinal speed of front car
+        acc_ego_max_resp: max acceleration of rear car during response time
+        acc_ego_min_brake: min braking of rear car --> ASSUMED SAME AS acc_ego_max_resp
+        acc_other_max_brake: max braking of front car --> ASSUMED SAME AS acc_ego_max_resp
+        """
+        # Constants
+        response_time = IDMVehicle.RESPONSE_TIME # Reaction time to begin braking
+        acc_ego_max_resp = IDMVehicle.COMFORT_ACC_MAX # Max acceleration of ego vehicle during response time (longitudinal)
+        acc_ego_min_brake = IDMVehicle.COMFORT_ACC_MAX # Min braking of ego vehicle
+        acc_other_max_brake = IDMVehicle.COMFORT_ACC_MAX # Max braking of other vehicle
+
+        # Ego and other vehicle velocities
+        v_ego = ego_vehicle.velocity  # Velocity of the rear vehicle (ego vehicle) --> array
+        v_other = other_vehicle.velocity  # Velocity of the front vehicle (other vehicle) --> array
+        # Ego vehicle speeds
+        v_ego_x = v_ego[0] # x-axis speed
+        # Other vehicle speeds
+        v_other_x = v_other[0] # x-axis speed
+
+        # Calculating safe longitudinal distance --> based on Eduardo/Leo's work (& Shalev-Shwartz, Shammah, and Shashua)
+        safe_lon_distance = v_ego_x * response_time + 0.5 * np.power(response_time, 2) * acc_ego_max_resp \
+                   + 0.5 * np.power(v_ego_x + response_time * acc_ego_max_resp, 2) / acc_ego_min_brake \
+                   - 0.5 * np.power(v_other_x, 2) / acc_other_max_brake
+
+        return max(safe_lon_distance, 0)
+    
+
+    def calculate_safe_lat_distance(self, ego_vehicle: Vehicle, other_vehicle: Vehicle):
+        """
+        Safe lateral distance 
+        *** All speed and acceleration inputs must be for the lateral axis
+        
+        Ego is assumed to be rear vehicle.
+        response_time: time it takes rear car to react and begin braking --> ASSUMED INSTANTANEOUS
+        ego_speed: current velocity of rear car
+        veh_speed: current velocity of front car
+        acc_rear_max_resp: max acceleration of rear car during response time
+        acc_rear_min_brake: min braking of rear car --> ASSUMED SAME AS BELOW
+        acc_front_max_brake: max braking of front car --> ASSUMED SAME AS ABOVE
+        """
+        # Constants
+        response_time = IDMVehicle.RESPONSE_TIME # Reaction time to begin braking
+        
+        # Ego and other vehicle velocities
+        v_ego = ego_vehicle.velocity  # Velocity of the rear vehicle (ego vehicle) --> array
+        v_other = other_vehicle.velocity  # Velocity of the front vehicle (other vehicle) --> array
+        # Ego vehicle speeds
+        v_ego_y = v_ego[1] # y-axis speed
+        # Other vehicle speeds
+        v_other_y = v_other[1] # y-axis speed
+
+        v_ego = v_ego_y + response_time * 1
+        v_veh = v_other_y - response_time * 1
+
+        # Calculating safe latitudinal distance --> based on Eduardo/Leo's work (& Shalev-Shwartz, Shammah, and Shashua)
+        safe_lat_distance = 0.5 * (v_ego_y + v_ego) * response_time + 0.5 * np.power(v_ego, 2) / 2 \
+                     - (0.5 * (v_other_y + v_veh) * response_time + 0.5 * np.power(v_veh, 2) / 2)
+
+        return max(safe_lat_distance, 0)
+    
+
+    def risk_index(self, safe_distance, distance):
+        """
+        Calculate the linear longitudinal or lateral risk index [0,1] *** All inputs must me either longitudinal or lateral.
+        safe_distance: safe longitudinal/lateral distance (use function SafeLonDistance/SafeLatDistance)
+        safe_distance_brake: safe longitudinal/lateral distance under max braking capacity
+                            (use function safe_lon_distance/safe_lat_distance with max braking acceleration)
+        distance: current longitudinal/lateral distance between cars
+        return: risk index [0,1]
+        """
+        if (distance > safe_distance):
+            r = 0
+        else:
+            r = 1 - distance / safe_distance
+
+        return r
+
+
+    def risk_index_unified(self, risk_lon, risk_prop_lon, risk_lat, risk_prop_lat):
+        """
+        Function to calculate the unified risk index [0,1]
+        risk_lon: longitudinal risk index (use function risk_index with longitudinal inputs)
+        risk_prop_lon: longitudinal risk propensity exponent > 0
+        risk_lat: lateral risk index (use function RiskIndex with lateral inputs)
+        risk_prop_lat: lateral risk propensity exponent > 0
+        return: unified risk index [0,1]
+        """
+        # Calculating unified risk index value --> based on Eduardo/Leo's work
+        r = np.power(risk_lon, risk_prop_lon) * np.power(risk_lat, risk_prop_lat)
+        
+        return r
+
+
     def calculate_TTC_near_miss(self):
         self.ttc_observation = TimeToCollisionObservation(self)
         TTC_grid = self.ttc_observation.observe_new()
@@ -291,18 +374,29 @@ class LLMEnv(AbstractEnv):
 
         return near_miss_occurred, nearest_vehicles
 
+
     def detect_edge_case(self, other_vehicle, near_miss_occurred):
+        # risk_prop_lon: longitudinal risk propensity exponent > 0
+        RISK_PROP_LON = 1
+        # risk_prop_lat: lateral risk propensity exponent > 0
+        RISK_PROP_LAT = 1
 
-        r_lon, r_lat = self.calculate_long_and_lat_risk_indices(self.vehicle, other_vehicle)
+        # Calculating current longitudinal and latitudinal distances
+        lon_distance_between_vehicles, lat_distance_between_vehicles = self.get_lon_and_lat_distance(self.vehicle, other_vehicle)
 
-        threshold_lon = 2.5  # High risk of front or rear collision
-        threshold_lat = 0.99  # High risk of side collision or lane departure
+        # Calculating safe longitudinal and latitudinal distances
+        safe_lon_distance = IDMVehicle.LENGTH + self.calculate_safe_lon_distance(self.vehicle, other_vehicle)
+        safe_lat_distance = IDMVehicle.WIDTH + self.calculate_safe_lat_distance(self.vehicle, other_vehicle)
+
+        r_lon = self.risk_index(safe_lon_distance, lon_distance_between_vehicles)
+        r_lat = self.risk_index(safe_lat_distance, lat_distance_between_vehicles)
+
+        r_unified = self.risk_index_unified(r_lon, RISK_PROP_LON, r_lat, RISK_PROP_LAT)
+
+        # print("This is the unified risk index", r_unified)
 
         # Define your edge case criteria        
-        is_edge_case_lon_and_lat = r_lon > threshold_lon or r_lat > threshold_lat
+        is_edge_case_lon_and_lat = r_unified >= 0.5
         is_edge_case_TTC_near_miss = near_miss_occurred
-
-        # print("Lat and long edge cases: ", is_edge_case_lon_and_lat)
-        # print("TTC-near-miss edge case: ", is_edge_case_TTC_near_miss)
 
         return is_edge_case_lon_and_lat, is_edge_case_TTC_near_miss
